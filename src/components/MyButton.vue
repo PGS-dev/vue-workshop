e82c09c50c4b4552666c5912539cd81eec95a5a6
LukaszKo <lukaszkochajewski@gmail.com>
2019-10-08 20:00:01 +0200

    - add sass

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -9,25 +9,24 @@
 <script>
 export default {};
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .btn {
   background-color: #f5f5f5;
   text-transform: uppercase;
   font-weight: 600;
   cursor: pointer;
   letter-spacing: 0.1rem;
-}
 
-.btn:hover {
-  background-color: #e0e0e0;
+  &:hover {
+    background-color: #e0e0e0;
+  }
 }
 
 .btn-action {
   color: #fff;
   background-color: #4caf50;
-}
-
-.btn-action:hover {
-  background-color: #66bb6a;
+  &:hover {
+    background-color: #66bb6a;
+  }
 }
 </style>
